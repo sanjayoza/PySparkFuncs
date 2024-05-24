e8a42d12caff74c78b4e2b48c68c9ec097c4925f
sanjayoza <45021769+sanjayoza@users.noreply.github.com>
2024-05-24 13:43:40 -0700

    Update helperFuncs.py
    Add default label and prediction column args

--- helperFuncs.py
+++ helperFuncs.py
@@ -1,16 +1,16 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-def getClassMetrics(preds):
+def getClassMetrics(preds, labelCol = 'label', predCol = 'prediction'):
     '''
     Pass in the predicition dataframe, dataset target column should be label and 
     the prediction column should be called prediction
     Returns: accuracy, precision, recall and f1-measure
     '''
-    tp = preds.filter((preds['label'] == 1) & (preds['prediction'] == 1)).count()
-    tn = preds.filter((preds['label'] == 0) & (preds['prediction'] == 0)).count()
-    fp = preds.filter((preds['label'] == 0) & (preds['prediction'] == 1)).count()
-    fn = preds.filter((preds['label'] == 1) & (preds['prediction'] == 0)).count()
+    tp = preds.filter((preds[labelCol] == 1) & (preds[predCol] == 1)).count()
+    tn = preds.filter((preds[labelCol] == 0) & (preds[predCol] == 0)).count()
+    fp = preds.filter((preds[labelCol] == 0) & (preds[predCol] == 1)).count()
+    fn = preds.filter((preds[labelCol] == 1) & (preds[predCol] == 0)).count()
 
     accuracy = (tp + tn) / (tp + tn + fp + fn)
     precision = tp / (tp + fp) if (tp + fp) != 0 else 0.0
