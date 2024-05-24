2f5d2dcbcd186f8835c7c4493f8ef5af23090358
sanjayoza <45021769+sanjayoza@users.noreply.github.com>
2024-05-24 13:06:40 -0700

    Update helperFuncs.py

--- helperFuncs.py
+++ helperFuncs.py
@@ -1,1 +1,20 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
+def getClassMetrics(preds):
+    '''
+    Pass in the predicition dataframe, dataset target column should be label and 
+    the prediction column should be called prediction
+    Returns: accuracy, precision, recall and f1-measure
+    '''
+    tp = preds.filter((preds['label'] == 1) & (preds['prediction'] == 1)).count()
+    tn = preds.filter((preds['label'] == 0) & (preds['prediction'] == 0)).count()
+    fp = preds.filter((preds['label'] == 0) & (preds['prediction'] == 1)).count()
+    fn = preds.filter((preds['label'] == 1) & (preds['prediction'] == 0)).count()
+
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp) if (tp + fp) != 0 else 0.0
+    recall = tp / (tp + fn) if (tp + fn) != 0 else 0.0
+    f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) != 0 else 0.0
+    
+    return accuracy, precision, recall, f1
